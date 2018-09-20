14cb0517cd7949738a854139f5b03e08ac133d18
Mogrady <mrogrady@att.net>
2018-09-19 20:57:01 -0400

    mongolab installation

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reddit";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/reddit", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/reddit", { useNewUrlParser: true });
 var db = require("./models");
 
 //web-scraping
